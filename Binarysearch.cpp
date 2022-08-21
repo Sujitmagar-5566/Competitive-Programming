4edec5d85f7e6fdec654a362a35f9b0d0f511eb0
Sujitmagar-5566 <111577839+Sujitmagar-5566@users.noreply.github.com>
2022-08-21 16:40:06 +0530

    Update Binarysearch.cpp

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -13,5 +13,15 @@ int main(){
     else {
         cout<<key<<" is not present";
     }
+    // get the index of the element 
+    // lower_bound(s,e,key) and upper_bound(s,e,key) 
+    // lower bound function returns location of first element >= key
+    // upper bound function returns location of first element strictly > key
+    auto lb= lower_bound(arr,arr+n,key);
+    cout<<(lb-arr)<<endl;
+    auto ub= upper_bound(arr,arr+n,key);
+    cout<<(ub-arr)<<endl;
+    // to find frequency of repeating element
+    cout<<"Occ freq of"<<key<<"is"<<(ub-lb)<<endl;
     return 0;
 }
