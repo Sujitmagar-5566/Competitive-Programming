223642c20149ccd34ea965b07ae0629425d9111b
Sujitmagar-5566 <111577839+Sujitmagar-5566@users.noreply.github.com>
2022-08-21 21:29:35 +0530

    Update Tokenizationstring.cpp

--- Tokenizationstring.cpp
+++ Tokenizationstring.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+   char s[100]= "Today is a rainy day" ;
+  char *ptr = strtok(s," ");
+  cout<<ptr<<endl; 
+  while(ptr!=NULL){
+    ptr= strtok(NULL," ");
+    cout<<ptr<<endl;
+  } 
+  return 0;
+}
